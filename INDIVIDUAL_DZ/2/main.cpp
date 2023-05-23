fbb0995c7cd9425cf4b2220a7dca1ba227d72673
StAl15 <stihinaleksander@yandex.ru>
2023-05-24 00:31:32 +0500

    individual dz 1

--- INDIVIDUAL_DZ/2/main.cpp
+++ INDIVIDUAL_DZ/2/main.cpp
@@ -2,63 +2,107 @@
 
 using namespace std;
 
-// Функция для вычисления множества цифр, входящих в запись числа
-int digits(int x) {
-    int d = 0;
-    while (x > 0) {
-        d |= 1 << (x % 10);
-        x /= 10;
+class MyString {
+private:
+    char *str;
+    int length;
+public:
+    MyString() : str(new char[1]), length(0) {
+        str[0] = '\0';
     }
-    return d;
-}
+
+    MyString(const char *s) : length(0) {
+        while (s[length] != '\0') {
+            ++length;
+        }
+        str = new char[length + 1];
+        for (int i = 0; i <= length; ++i) {
+            str[i] = s[i];
+        }
+    }
+
+    MyString(const MyString &other) : length(other.length), str(new char[length + 1]) {
+        for (int i = 0; i <= length; ++i) {
+            str[i] = other.str[i];
+        }
+    }
+
+    ~MyString() {
+        delete[] str;
+    }
+
+    int getLength() const {
+        return length;
+    }
+
+    const char *getStr() const {
+        return str;
+    }
+
+    MyString &operator=(const MyString &other) {
+        if (this != &other) {
+            MyString temp(other);
+            char *tempStr = str;
+            str = temp.str;
+            temp.str = tempStr;
+            length = other.length;
+        }
+        return *this;
+    }
+
+    MyString operator+(const MyString &other) const {
+        MyString result;
+        result.length = length + other.length;
+        result.str = new char[result.length + 1];
+        for (int i = 0; i < length; ++i) {
+            result.str[i] = str[i];
+        }
+        for (int i = 0; i <= other.length; ++i) {
+            result.str[length + i] = other.str[i];
+        }
+        return result;
+    }
+
+    MyString substr(int k, int p) const {
+        if (p < 0 || p >= length || k <= 0) {
+            return MyString();
+        }
+        int len = (k < length - p) ? k : length - p;
+        MyString result;
+        result.length = len;
+        result.str = new char[len + 1];
+        for (int i = 0; i < len; ++i) {
+            result.str[i] = str[p + i];
+        }
+        result.str[len] = '\0';
+        return result;
+    }
+};
 
 int main() {
-    int n, k;
-    cin >> n >> k;
-    int a[32];
-    for (int i = 0; i < n; i++) {
-        cin >> a[i];
-    }
+    // тестирование конструкторов и методов класса
+    MyString s1("Hello");
+    MyString s2(" world");
+    MyString s3 = s1 + s2;
+    cout << "s1: " << s1.getStr() << endl;
+    cout << "s2: " << s2.getStr() << endl;
+    cout << "s3: " << s3.getStr() << endl;
+    MyString s4 = s3.substr(5, 4);
+    cout << "s4: " << s4.getStr() << endl;
 
-    // Вычисление множеств цифр для всех чисел
-    int masks[32];
-    for (int i = 0; i < n; i++) {
-        masks[i] = digits(a[i]);
-    }
+    // тестирование операторов
+    MyString s5("foo");
+    MyString s6("bar");
+    MyString s7 = s5 + s6;
+    cout << "s5 + s6 = " << s7.getStr() << endl;
 
-    // Перебор всех возможных подмножеств чисел
-    for (int mask = 0; mask < (1 << n); mask++) {
-        int count = 0;
-        for (int i = 0; i < n; i++) {
-            if (mask & (1 << i)) {
-                count++;
-            }
-        }
-        if (count == k) { // Если подмножество имеет k элементов
-            bool is_same_digits = true;
-            int d = 0;
-            for (int i = 0; i < n; i++) {
-                if (mask & (1 << i)) { // Если i-й элемент входит в подмножество
-                    if (d == 0) { // Если это первый элемент, записываем его множество цифр
-                        d = masks[i];
-                    } else {
-                        if (d != masks[i]) { // Если множества цифр не совпадают, прерываем проверку
-                            is_same_digits = false;
-                            break;
-                        }
-                    }
-                }
-            }
-            if (is_same_digits) { // Если все числа в подмножестве имеют одинаковые множества цифр, выводим его
-                for (int i = 0; i < n; i++) {
-                    if (mask & (1 << i)) {
-                        cout << a[i] << " ";
-                    }
-                }
-                cout << endl;
-            }
-        }
-    }
+    s5 = s6;
+    cout << "s5 = s6, s5: " << s5.getStr() << endl;
+
+    // тестирование выделения подстроки
+    MyString s8("Hello, world!");
+    MyString s9 = s8.substr(7, 5);
+    cout << "s9: " << s9.getStr() << endl;
 
     return 0;
 }
